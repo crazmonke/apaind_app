c0758c5308dfbe34bb951baf87217eeb6ee25596
yosep.kang <yosep.kang@nts-corp.com>
2026-08-21 14:35:51 +0900

    이메일 주소 변경

--- apt_community_app/lib/app_config.dart
+++ apt_community_app/lib/app_config.dart
@@ -3,4 +3,4 @@ const String kSupportCenterUrl = 'https://apaind.cloud/support';
 const String kSupportCenterUrl1 = 'https://apaind.cloud/reports/new';
 const String kTermsOfUseUrl = 'https://apaind.cloud/posts/14';
 const String kPrivacyPolicyUrl = 'https://apaind.cloud/posts/13';
-const String kSupportEmail = 'kysloving@gmail.com';
+const String kSupportEmail = 'apaind@apaind.cloud';
